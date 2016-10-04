3b7eaee48aa01d79e5153c4c5e53c2eabd775c24
Johanne Demetria <jod.demetria@gmail.com>
2016-10-04 17:42:03 +0800

    Initial Commit - test

---
+++ GoogleFoobar/src/com/google/challenges/Answer.java
@@ -0,0 +1,10 @@
+package com.google.challenges;
+
+public class Answer {
+    public static String answer(String M, String F) { 
+    	String retVal = "impossible";
+    	
+        // Your code goes here.
+    	return retVal;
+    } 
+}
